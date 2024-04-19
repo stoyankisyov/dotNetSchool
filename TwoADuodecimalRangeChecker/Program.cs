7a40b2953ca722b060bc55ddc763877ed8e702ed
Stoyan Kisyov <work.stoyan.kisyov@gmail.com>
2024-04-19 20:20:10 +0300

    Final variant of the task, code polished

--- TwoADuodecimalRangeChecker/Program.cs
+++ TwoADuodecimalRangeChecker/Program.cs
@@ -6,12 +6,14 @@
         {
             Console.Write($"Enter value for a: ");
             var a = int.Parse(Console.ReadLine());
+
             Console.Write($"Enter value for b: ");
             var b = int.Parse(Console.ReadLine());
 
             for (int i = a; i <= b; i++)
             {
                 var duodecimalNumber = ConvertDecimalToDuodecimal(i);
+
                 if (HasTwoAs(duodecimalNumber))
                 {
                     Console.WriteLine($"{i} - {duodecimalNumber}");
@@ -21,15 +23,12 @@
 
         private static string ConvertDecimalToDuodecimal(int decimalNumber)
         {
-            // var duodecimalDigits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B' };
             var result = "";
 
             while (decimalNumber > 0)
             {
                 var remainder = decimalNumber % 12;
-                // result = duodecimalDigits[remainder] + result; 
-                // switch-case lesser chance for bugs, since not using array and you cannot change the array's elements by mistake, not relying on accessing by index
-                // + improved readability
+
                 switch (remainder)
                 {
                     case 0:
@@ -57,14 +56,17 @@
                     case 11:
                         result = "B" + result; break;
                 }
+
                 decimalNumber /= 12;
             }
+
             return result;
         }
 
         private static bool HasTwoAs(string duodecimalNumber)
         {
             var count = 0;
+
             foreach (var digint in duodecimalNumber)
             {
                 if (digint == 'A')
@@ -72,6 +74,7 @@
                     count++;
                 }
             }
+
             return count == 2;
         }
     }
