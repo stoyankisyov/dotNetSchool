933c337c7922a88d910218775026194b2f63e9a5
Stoyan Kisyov <work.stoyan.kisyov@gmail.com>
2024-04-26 17:33:26 +0300

    Task solved + UnitTests

---
+++ DiagonalMatrix/DiagonalMatrix.cs
@@ -0,0 +1,98 @@
+﻿namespace DiagonalMatrixNamespace
+{
+    public class DiagonalMatrix
+    {
+        public int[] DiagonalElements { get; set; }
+        public int Size { get; }
+
+        public DiagonalMatrix(params int[] matrixElements)
+        {
+            if (matrixElements is null)
+            {
+                DiagonalElements = [];
+                Size = 0;
+            }
+            else
+            {
+                Size = matrixElements.Length;
+                DiagonalElements = new int[Size];
+                Array.Copy(matrixElements, DiagonalElements, Size);
+            }
+        }
+
+        public int this[int i, int j]
+        {
+            get => IsElementOnMainDiagonal(i, j) ? DiagonalElements[i] : 0;
+            set
+            {
+                if (IsElementOnMainDiagonal(i, j))
+                {
+                    DiagonalElements[i] = value;
+                }
+            }
+        }
+
+        public int Track()
+        {
+            var diagonalMatrixElementsSum = 0;
+
+            foreach (var element in DiagonalElements)
+            {
+                diagonalMatrixElementsSum += element;
+            }
+
+            return diagonalMatrixElementsSum;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is DiagonalMatrix matrixToCompare && matrixToCompare.Size == Size)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    if (DiagonalElements[i] != matrixToCompare.DiagonalElements[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            foreach (var element in DiagonalElements)
+            {
+                hash = hash * 31 + element.GetHashCode();
+            }
+
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            var result = string.Empty;
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    result += this[i, j].ToString() + " ";
+                }
+                result += "\n";
+            }
+
+            return result;
+        }
+
+        private bool IsElementOnMainDiagonal(int i, int j)
+        {
+            return i == j && i >= 0 && i < Size;
+        }
+    }
+}
---
+++ DiagonalMatrix/DiagonalMatrixExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace DiagonalMatrixNamespace
+{
+    public static class DiagonalMatrixExtensions
+    {
+        public static DiagonalMatrix Addition(this DiagonalMatrix firstDiagonalMatrix, DiagonalMatrix secondDiagonalMatrix)
+        {
+            var greaterSize = Math.Max(firstDiagonalMatrix.Size, secondDiagonalMatrix.Size);
+            var newDiagonalMatrix = new int[greaterSize];
+
+            for(int i = 0; i < greaterSize; i++)
+            {
+                var firstDiagonalMatrixValue = i < firstDiagonalMatrix.Size ? firstDiagonalMatrix.DiagonalElements[i] : 0;
+                var secondDiagonalMatrixValue = i < secondDiagonalMatrix.Size ? secondDiagonalMatrix.DiagonalElements[i] : 0;
+
+                newDiagonalMatrix[i] = firstDiagonalMatrixValue + secondDiagonalMatrixValue;
+            }
+
+            return new DiagonalMatrix(newDiagonalMatrix);
+        }
+    }
+}
