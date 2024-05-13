578459c3be2aa8a218bd54a33826e807644d82a6
Stoyan Kisyov <work.stoyan.kisyov@gmail.com>
2024-05-13 21:55:37 +0300

    Task solved + unit tests, all passing

---
+++ RationalNumbers/RationalNumber.cs
@@ -0,0 +1,96 @@
+﻿namespace RationalNumbers
+{
+    public sealed class RationalNumber : IComparable<RationalNumber>
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+
+        public RationalNumber(int numerator, int denumerator)
+        {
+            if (denumerator == 0)
+            {
+                throw new ArgumentException("Denumerator can't be 0!");
+            }
+
+            int greatestCommonDivisor = GetGreatestCommonDivisor(numerator, denumerator);
+            Numerator = numerator / greatestCommonDivisor;
+            Denominator = denumerator / greatestCommonDivisor;
+
+            if (Denominator < 0)
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is RationalNumber otherNumber)
+            {
+                return Numerator == otherNumber.Numerator && Denominator == otherNumber.Denominator;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
+        }
+
+        public int CompareTo(RationalNumber? other)
+        {
+            if (other is null)
+            {
+                return 1;
+            }
+
+            return (Numerator * other.Denominator).CompareTo(Denominator * other.Numerator);
+        }
+
+        public static RationalNumber operator +(RationalNumber left, RationalNumber right)
+        {
+            return new RationalNumber(left.Numerator * right.Denominator + right.Numerator * left.Denominator, left.Denominator * right.Denominator);
+        }
+
+        public static RationalNumber operator -(RationalNumber left, RationalNumber right)
+        {
+            return new RationalNumber(left.Numerator * right.Denominator - right.Numerator * left.Denominator, left.Denominator * right.Denominator);
+        }
+
+        public static RationalNumber operator *(RationalNumber left, RationalNumber right)
+        {
+            return new RationalNumber(left.Numerator * right.Numerator, left.Denominator * right.Denominator);
+        }
+
+        public static RationalNumber operator /(RationalNumber left, RationalNumber right)
+        {
+            if (right.Numerator == 0)
+            {
+                throw new DivideByZeroException("Can't divide by 0!");
+            }
+
+            return new RationalNumber(left.Numerator * right.Denominator, left.Denominator * right.Numerator);
+        }
+
+        public static explicit operator double(RationalNumber number)
+        {
+            return (double)number.Numerator / number.Denominator;
+        }
+
+        public static implicit operator RationalNumber(int number)
+        {
+            return new RationalNumber(number, 1);
+        }
+
+        private static int GetGreatestCommonDivisor(int firstNumber, int secondNumber)
+        {
+            return secondNumber == 0 ? Math.Abs(firstNumber) : GetGreatestCommonDivisor(secondNumber, firstNumber % secondNumber);
+        }
+    }
+}
