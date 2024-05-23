e484c457c8c1872663c5ce4e694277788d96f414
Stoyan Kisyov <work.stoyan.kisyov@gmail.com>
2024-05-23 13:32:26 +0300

    Tests fixed

--- BookCatalogTests/BookHelperTests.cs
+++ BookCatalogTests/BookHelperTests.cs
@@ -8,29 +8,23 @@ namespace BookCatalogTests
         [TestMethod]
         public void UnifyIsbnSuccess()
         {
-            var bookHelper = new BookHelper();
-
-            Assert.AreEqual("1234567891234", bookHelper.UnifyIsbn("123-4-56-789123-4"));
+            Assert.AreEqual("1234567891234", BookHelper.UnifyIsbn("123-4-56-789123-4"));
         }
 
         [TestMethod]
         public void IsIsbnCorrectFormatSuccess()
         {
-            var bookHelper = new BookHelper();
-
-            Assert.IsTrue(bookHelper.IsIsbnInCorrectFormat("123-4-56-789123-4"));
-            Assert.IsTrue(bookHelper.IsIsbnInCorrectFormat("1234567891234"));
+            Assert.IsTrue(BookHelper.IsIsbnInCorrectFormat("123-4-56-789123-4"));
+            Assert.IsTrue(BookHelper.IsIsbnInCorrectFormat("1234567891234"));
         }
 
         [TestMethod]
         public void IsIsbnCorrectFormatFail()
         {
-            var bookHelper = new BookHelper();
-
-            Assert.IsFalse(bookHelper.IsIsbnInCorrectFormat("123-4"));
-            Assert.IsFalse(bookHelper.IsIsbnInCorrectFormat("1-2-3-4-5-6-7-8-9-1-2-3-4"));
-            Assert.IsFalse(bookHelper.IsIsbnInCorrectFormat("1#2-3-4@5-6-7-8A1-2-B-4"));
-            Assert.IsFalse(bookHelper.IsIsbnInCorrectFormat("  "));
+            Assert.IsFalse(BookHelper.IsIsbnInCorrectFormat("123-4"));
+            Assert.IsFalse(BookHelper.IsIsbnInCorrectFormat("1-2-3-4-5-6-7-8-9-1-2-3-4"));
+            Assert.IsFalse(BookHelper.IsIsbnInCorrectFormat("1#2-3-4@5-6-7-8A1-2-B-4"));
+            Assert.IsFalse(BookHelper.IsIsbnInCorrectFormat("  "));
         }
     }
 }
